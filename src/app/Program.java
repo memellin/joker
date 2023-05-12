acd18a6de243e00554757581a80e4a64b01de865
memellin <enocksm@outlook.com>
2023-05-12 18:22:57 -0300

    Coringa

---
+++ src/app/Program.java
@@ -0,0 +1,27 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Circle;
+import entities.Rectangle;
+import entities.Shape;
+
+public class Program {
+	public static void main(String[] argrs) {
+			
+		List<Shape> myShapes = new ArrayList<>();
+		myShapes.add(new Rectangle(3.0, 2.0));
+		myShapes.add(new Circle(2.0));
+		
+		System.out.println("Total area: " + totalArea(myShapes));
+	}
+	
+	public static double totalArea(List<Shape> list) {
+		double sum = 0.0;
+		for( Shape s : list) {
+			sum += s.area();
+		}
+		return sum;
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author enock
+ *
+ */
+module joker {
+}
